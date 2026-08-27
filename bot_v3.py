cf5a22c268442e36c1584897d4825b081101fc7b
Tricorniometria <tricorniometria@gmail.com>
2026-08-28 00:14:18 +0200

    Update bot_v3.py

--- bot_v3.py
+++ bot_v3.py
@@ -30,8 +30,9 @@ def enviar_telegram(mensaje):
     respuesta = requests.post(
         url,
         data={"chat_id": chat_id, "text": mensaje},
-        timeout=20,
-    )    if not respuesta.ok:
+                timeout=20,
+    )
+    if not respuesta.ok:
         print("ERROR EXACTO DE TELEGRAM:", respuesta.status_code, respuesta.text)
     respuesta.raise_for_status()
 
